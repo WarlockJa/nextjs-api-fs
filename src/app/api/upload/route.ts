5af169ad2283e4c38d5959e5679d542130a678c8
warlockja <warlockja@gmail.com>
2023-09-12 10:35:01 +0300

    testing

--- src/app/api/upload/route.ts
+++ src/app/api/upload/route.ts
@@ -19,29 +19,10 @@ export async function POST(req: NextRequest) {
   // writing file
   const path = `/tmp/${file.name}`;
 
-  await writeFile(path, buffer);
-  return NextResponse.json({ success: true, path });
+  try {
+    await writeFile(path, buffer);
+    return NextResponse.json({ success: true, path });
+  } catch (error) {
+    return NextResponse.json({ success: false, error });
+  }
 }
-
-// import { writeFile } from "fs/promises";
-// import { NextRequest, NextResponse } from "next/server";
-
-// export async function POST(request: NextRequest) {
-//   const data = await request.formData();
-//   const file: File | null = data.get("file") as unknown as File;
-
-//   if (!file) {
-//     return NextResponse.json({ success: false });
-//   }
-
-//   const bytes = await file.arrayBuffer();
-//   const buffer = Buffer.from(bytes);
-
-//   // With the file data in the buffer, you can do whatever you want with it.
-//   // For this, we'll just write it to the filesystem in a new location
-//   const path = `/tmp/${file.name}`;
-//   await writeFile(path, buffer);
-//   console.log(`open ${path} to see the uploaded file`);
-
-//   return NextResponse.json({ success: true });
-// }
